748b94b180e69a557e656d15741d8cfab72d31db
hamzarezig <rezig3791@gmail.com>
2023-11-08 14:28:15 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+def isSafe(cmd):
+    blacklist = [" "]
+    for i in blacklist:
+        if i in cmd:
+            return False
+    return True
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    image = None
+    if request.method == 'POST':
+      if isSafe(request.form.get('command')):
+        command = "ping "+request.form.get('command')
+        result = subprocess.getoutput(command)
+      else:
+        image = "no.jpg"
+        result = "...."
+    return render_template('command.html', result=result,image=image)
+
+if __name__ == '__main__':
+    app.run()
